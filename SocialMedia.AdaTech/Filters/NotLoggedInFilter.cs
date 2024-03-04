1a2d9dc3310d2158ab3e028618172d739ee1e9ed
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-03-04 00:25:43 -0300

    add auth filters

---
+++ SocialMedia.AdaTech/Filters/NotLoggedInFilter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace SocialMedia.AdaTech.Filters
+{
+    public class NotLoggedInFilter : IAuthorizationFilter
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public NotLoggedInFilter(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (_httpContextAccessor.HttpContext.Request.Cookies.ContainsKey("jwt"))
+            {
+                context.Result = new ContentResult
+                {
+                    Content = "Usuário já está logado.",
+                    StatusCode = StatusCodes.Status401Unauthorized,
+                    ContentType = "text/plain"
+                };
+            }
+        }
+    }
+}
---
+++ SocialMedia.AdaTech/Filters/VerifyJWTFilter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace SocialMedia.AdaTech.Filters
+{
+    public class VerifyJWTFilter : IAuthorizationFilter
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public VerifyJWTFilter(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!_httpContextAccessor.HttpContext.Request.Cookies.ContainsKey("jwt"))
+            {
+                context.Result = new ContentResult
+                {
+                    Content = "Usuário precisa estar logado para ter acesso a essa rota.",
+                    StatusCode = StatusCodes.Status401Unauthorized,
+                    ContentType = "text/plain"
+                };
+            }
+        }
+    }
+}
