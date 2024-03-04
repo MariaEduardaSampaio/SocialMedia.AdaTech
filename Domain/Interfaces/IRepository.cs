106658aa1be1768a7ed988d6f87abe7ab472c90e
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-03-03 22:55:45 -0300

    add signatures in IRepository

--- Domain/Interfaces/IRepository.cs
+++ Domain/Interfaces/IRepository.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Entities;
 
 namespace Domain.Interfaces
 {
     public interface IRepository
     {
+        User AddUser(User user);
+        User? ReadUserByName(string name);
+        User? ReadUserByEmail(string email);
+        User? ReadUserByID(int id);
+        IEnumerable<User>? ReadAllUsers();
+        User? UpdateUser(UserRequest request, int id);
+        User? DeleteUser(int id);
     }
 }
