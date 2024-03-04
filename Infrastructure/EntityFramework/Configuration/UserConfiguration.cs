52fa7c31ca9d44ae1938f27d832f642235304b94
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-03-03 22:56:12 -0300

    add user configuration

---
+++ Infrastructure/EntityFramework/Configuration/UserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.EntityFramework.Configuration
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(l => l.Id);
+            builder.Property(l => l.Id).ValueGeneratedOnAdd().UseIdentityColumn();
+            builder.Property(l => l.Name).IsRequired();
+            builder.Property(l => l.Email).IsRequired();
+            builder.Property(l => l.Password).IsRequired();
+        }
+    }
+}
